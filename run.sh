3a705c4879d764357888b5a906e60a3215895d63
mikesimonsen <mike.simonsen@compass.com>
2026-03-20 14:59:57 -0700

    Add start/finish timestamps to run.sh for debugging

--- run.sh
+++ run.sh
@@ -3,5 +3,7 @@
 cd /Users/mikesimonsen/projects/econ-data
 LOG_DIR=logs
 mkdir -p "$LOG_DIR"
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] run.sh started" >> "$LOG_DIR/run.log"
 PYTHONWARNINGS=ignore /Users/mikesimonsen/projects/econ-data/.venv/bin/python run.py \
     >> "$LOG_DIR/run.log" 2>&1
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] run.sh finished (exit=$?)" >> "$LOG_DIR/run.log"
